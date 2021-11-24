5a4c22b45b5ab14d7ad1133292db9b889fe55c24
ygo hamon <ygohamonsh@gmail.com>
2021-11-24 16:03:27 -0300

    V2.1

--- App.js
+++ App.js
@@ -1,144 +1,182 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronRight,
+  faChevronLeft,
+  faCircle,
+  faCheckCircle,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
 
 const App = () => {
-
   const [items, setItems] = useState([]);
-	const [inputValue, setInputValue] = useState();
-	const [totalItemCount, setTotalItemCount] = useState(0);
-	
-	const handleAdd = () => {
+  const [inputValue, setInputValue] = useState();
+  const [totalItemCount, setTotalItemCount] = useState(0);
 
-		if(inputValue != null){
+  const handleAdd = () => {
+    if (inputValue != null) {
+      const newItem = {
+        itemName: inputValue,
+        quantity: 0,
+        valor: null,
+        isSelected: false,
+      };
 
-			const newItem = {
-				itemName: inputValue,
-				quantity: 0,
-				valor: null,
-				isSelected: false,
-			};
+      const newItems = [...items, newItem];
 
-			const newItems = [...items, newItem];
+      setItems(newItems);
+      setInputValue("");
+      calculateTotal();
+    } else {
+      alert(
+        "O campo precisa de no minimo 3 caracteres para serem inseridos!!!"
+      );
+    }
+  };
 
-			setItems(newItems);
-			setInputValue('');
-			calculateTotal();
+  const handleQuantityIncrease = (index) => {
+    const newItems = [...items];
 
-		}else{
-			alert("O campo precisa de no minimo 3 caracteres para serem inseridos!!!");
-		}
+    newItems[index].quantity++;
 
-	};
+    setItems(newItems);
+    calculateTotal();
+  };
 
-	const handleQuantityIncrease = (index) => {
-		const newItems = [...items];
+  const handleQuantityDecrease = (index) => {
+    const newItems = [...items];
 
-		newItems[index].quantity++;
+    newItems[index].quantity--;
 
-		setItems(newItems);
-		calculateTotal();
-	};
+    setItems(newItems);
+    calculateTotal();
+  };
 
-	const handleQuantityDecrease = (index) => {
-		const newItems = [...items];
+  const toggleComplete = (index) => {
+    const newItems = [...items];
 
-		newItems[index].quantity--;
+    newItems[index].isSelected = !newItems[index].isSelected;
 
-		setItems(newItems);
-		calculateTotal();
-	};
+    setItems(newItems);
+  };
 
-	const toggleComplete = (index) => {
-		const newItems = [...items];
+  const calculateTotal = () => {
+    const totalItemCount = items.reduce((total, item) => {
+      return total + item.quantity;
+    }, 0);
 
-		newItems[index].isSelected = !newItems[index].isSelected;
+    setTotalItemCount(totalItemCount);
+  };
 
-		setItems(newItems);
-	};
+  const handleChangeValueItem = (index, value) => {
+    const _items = [...items];
+    _items[index].valor = value;
 
-	const calculateTotal = () => {
-		const totalItemCount = items.reduce((total, item) => {
-			return total + item.quantity;
-		}, 0);
+    setItems(_items);
+  };
 
-		setTotalItemCount(totalItemCount);
-	};
+  var total = items.reduce(getTotal, 0);
 
-	const handleChangeValueItem = (index, value) => {
-		const _items = [...items];
-		_items[index].valor = value;
-
-		setItems(_items);
-	}
-
-	var total = items.reduce(getTotal, 0);
-
-	function getTotal(total, item) {
-		return total + (item.valor * item.quantity);
-	};
+  function getTotal(total, item) {
+    return total + item.valor * item.quantity;
+  }
 
   return (
     <View style={styles.container}>
+      <View style={styles.main_container}>
+        <View style={styles.add_item_box}>
+          <TextInput
+            style={styles.add_item_input}
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+            placeholder="Adicione o produto..."
+          />
+          <FontAwesomeIcon icon={faPlus} onClick={() => handleAdd()} />
+        </View>
+        <View style={styles.item_list}>
+          {items.map((item, index) => (
+            <View key={index} style={styles.item_container}>
+              <View
+                onTouchStart={() => toggleComplete(index)}
+                style={styles.item_name}
+              >
+                {item.isSelected ? (
+                  <React.Fragment>
+                    <FontAwesomeIcon icon={faCheckCircle} />
+                    <Text style={styles.complete}>{item.itemName}</Text>
+                  </React.Fragment>
+                ) : (
+                  <React.Fragment>
+                    <FontAwesomeIcon icon={faCircle} />
+                    <Text>{item.itemName}</Text>
+                  </React.Fragment>
+                )}
+              </View>
 
-      <TextInput value={inputValue} onChange={(event) => setInputValue(event.target.value)} placeholder='Adicione o produto...'/>
-      <FontAwesomeIcon icon={faPlus} onClick={() => handleAdd()} />
-	  
-	  <View>
-        {items.map((item, index) => (
-          <View key={index}>
-            <View onTouchStart={() => toggleComplete(index)}>
+              <View style={styles.quantity}>
+                <View style={styles.valor_item_box}>
+                  R$:
+                  <TextInput
+                    placeholder="0,00"
+                    value={item.valor}
+                    onChange={(event) => {
+                      handleChangeValueItem(index, event.currentTarget.value);
+                    }}
+					style={styles.valor_item_input}
+                  />
+                </View>
 
-              {item.isSelected ? (
+                <Button onPress={() => handleQuantityDecrease(index)} title="-">
+                  <FontAwesomeIcon
+                    icon={faChevronLeft}
+                    onPress={() => handleQuantityDecrease(index)}
+                  />
+                </Button>
 
-                <React.Fragment>
-				<FontAwesomeIcon icon={faCheckCircle} />
-				<Text>{item.itemName}</Text>
-                </React.Fragment>
+                <Text>{item.quantity}</Text>
 
-              ) : (
-
-                <React.Fragment>
-				<FontAwesomeIcon icon={faCircle} />
-				<Text>{item.itemName}</Text>
-                </React.Fragment>
-
-              )}
-
+                <Button onPress={() => handleQuantityIncrease(index)} title="+">
+                  <FontAwesomeIcon
+                    icon={faChevronRight}
+                    onClick={() => handleQuantityIncrease(index)}
+                  />
+                </Button>
+              </View>
             </View>
-
-            <View>
-            	R$ <TextInput placeholder='0,00' value={item.valor} onChange={(event) => { handleChangeValueItem(index, event.currentTarget.value) }}/>
-            
-			</View>
-			  	<Button onPress={() => handleQuantityDecrease(index)} title="-">
-				  <FontAwesomeIcon icon={faChevronLeft} onPress={() => handleQuantityDecrease(index)} />
-			  	</Button>
-
-              		<Text>{item.quantity}</Text>
-
-              	<Button onPress={() => handleQuantityIncrease(index)} title="+">
-				  <FontAwesomeIcon icon={faChevronRight} onClick={() => handleQuantityIncrease(index)} />
-				</Button>
-          </View>
-
-        ))}
-        <View>Qtotal: {totalItemCount}</View>
-        <View>R$: {total.toFixed(2)}</View>
+          ))}
+        </View>
+        <View style={styles.total}>Qtotal: {totalItemCount}</View>
+        <View style={styles.valor}>R$: {total.toFixed(2)}</View>
       </View>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#6554bb",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  app_background: {
+    backgroundColor: "6554bb",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    innerHeight: "100",
+  },
+  main_container: {
+    backgroundColor: "#f5f6fa",
+    width: "500px",
+    height: "min-content",
+    borderradius: "5px",
+    padding: "20px",
+    boxshadow: "10px 10px 26px 0px rgba(0, 0, 0, 0.35)",
   },
 });
 
