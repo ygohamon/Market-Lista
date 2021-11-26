398eb945ac05d281cb571944e157a5360dfeaf3b
Ygo Hamon <31461178+ygohamon10@users.noreply.github.com>
2021-11-25 22:52:05 -0300

    V3.0

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle, faCheckCircle, faPlus, faPlusCircle, faMinusCircle } from "@fortawesome/free-solid-svg-icons";
 
@@ -11,24 +11,28 @@ const App = () => {
   const [totalItemCount, setTotalItemCount] = useState(0);
 
   const handleAdd = () => {
-    if (inputValue != null) {
-      const newItem = {
-        itemName: inputValue,
-        quantity: 0,
-        valor: null,
-        isSelected: false,
-      };
 
-      const newItems = [...items, newItem];
+    const newItem = {
+      itemName: inputValue,
+      quantity: 0,
+      valor: null,
+      isSelected: false,
+    };
 
-      setItems(newItems);
-      setInputValue("");
-      calculateTotal();
-    } else {
-      alert(
-        "O campo precisa de no minimo 3 caracteres para serem inseridos!!!"
-      );
-    }
+    const newItems = [...items, newItem];
+
+    setItems(newItems);
+    setInputValue("");
+    calculateTotal();
+
+  };
+
+  const completeTask = (index) => {
+    const newItems = [...items];
+
+    newItems[index].isSelected = !newItems[index].isSelected;
+
+    setItems(newItems);
   };
 
   const handleQuantityIncrease = (index) => {
@@ -49,12 +53,11 @@ const App = () => {
     calculateTotal();
   };
 
-  const toggleComplete = (index) => {
-    const newItems = [...items];
+  const handleChangeValueItem = (index, value) => {
+    const _items = [...items];
+    _items[index].valor = value;
 
-    newItems[index].isSelected = !newItems[index].isSelected;
-
-    setItems(newItems);
+    setItems(_items);
   };
 
   const calculateTotal = () => {
@@ -65,215 +68,212 @@ const App = () => {
     setTotalItemCount(totalItemCount);
   };
 
-  const handleChangeValueItem = (index, value) => {
-    const _items = [...items];
-    _items[index].valor = value;
-
-    setItems(_items);
-  };
 
   var total = items.reduce(getTotal, 0);
 
   function getTotal(total, item) {
     return total + item.valor * item.quantity;
-  }
+  };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.main_container}>
-        <View style={styles.add_item_box}>
-          <TextInput
-            style={styles.add_item_input}
-            value={inputValue}
-            onChange={(event) => setInputValue(event.target.value)}
-            placeholder="Adicione o produto..."
-          />
-          <FontAwesomeIcon icon={faPlus} onClick={() => handleAdd()} />
-        </View>
-        <View style={styles.item_list}>
-          {items.map((item, index) => (
-            <View key={index} style={styles.item_container}>
-              <View
-                onClick={() => toggleComplete(index)}
-                style={styles.item_name}
-              >
-                {item.isSelected ? (
-                  <React.Fragment>
-                    <FontAwesomeIcon icon={faCheckCircle} />
-                    <Text style={styles.completed}>{item.itemName}</Text>
-                  </React.Fragment>
-                ) : (
-                  <React.Fragment>
-                    <FontAwesomeIcon icon={faCircle} />
-                    <Text>{item.itemName}</Text>
-                  </React.Fragment>
-                )}
-              </View>
 
-              <View style={styles.quantity}>
-                <View style={styles.valor_item_box}>
-                  R$:
-                  <TextInput
-                    placeholder="0,00"
-                    value={item.valor}
-                    onChange={(event) => {
-                      handleChangeValueItem(index, event.currentTarget.value);
-                    }}
-					          style={styles.valor_item_input}
-                  />
-                </View>
-                  <FontAwesomeIcon
-                    icon={faPlusCircle}
-                    onClick={() => handleQuantityIncrease(index)}
-                  />
-                <Text>{item.quantity}</Text>
-                  <FontAwesomeIcon
-                    icon={faMinusCircle}
-                    onClick={() => handleQuantityDecrease(index)}
-                  />
-                
-              </View>
-            </View>
-          ))}
+    <View style={styles.container}>
+
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1
+        }}
+        keyboardShouldPersistTaps='handled'
+      >
+        <View style={styles.tasksWrapper}>
+          <Text style={styles.sectionTitle}>Lista da Feira do dia</Text>
+
+          <View >Qtotal: {totalItemCount}</View>
+          <View >R$: {total.toFixed(2)}</View>
+
+          <View style={styles.items}>
+            {
+              items.map((item, index) => {
+                return (
+
+                  <View style={styles.item}>
+                    <TouchableOpacity onPress={() => completeTask(index)}>
+                      <View style={styles.itemLeft}>
+                        {item.isSelected ? (
+                          <React.Fragment>
+                            <FontAwesomeIcon icon={faCheckCircle} />
+                            <Text style={styles.itemText2}>{item.itemName}</Text>
+                          </React.Fragment>
+                        ) : (
+                          <React.Fragment>
+                            <FontAwesomeIcon icon={faCircle} />
+                            <Text style={styles.itemText}> {item.itemName}</Text>
+                          </React.Fragment>
+                        )}
+                      </View>
+                    </TouchableOpacity>
+                    <View style={styles.quantity}>
+                    <View style={styles.valor_box}>
+                      
+                      R$:
+                    <TextInput
+                      placeholder="0,00"
+                      value={item.valor}
+                      onChange={(event) => {
+                        handleChangeValueItem(index, event.currentTarget.value);
+                      }}
+                      style={styles.valor}
+                    />
+                    
+                    </View>
+                    <FontAwesomeIcon
+                      icon={faPlusCircle}
+                      onClick={() => handleQuantityIncrease(index)}
+                    />
+                    <Text>{item.quantity}</Text>
+                    <FontAwesomeIcon
+                      icon={faMinusCircle}
+                      onClick={() => handleQuantityDecrease(index)}
+                    />
+                    </View>
+                    </View>
+                  
+
+
+                )
+              })
+            }
+          </View>
         </View>
-        <View style={styles.total}>Qtotal: {totalItemCount}</View>
-        <View style={styles.valor}>R$: {total.toFixed(2)}</View>
+      </ScrollView >
+
+      <View
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+          placeholder="Adicione o produto..."
+        />
+        
+        <TouchableOpacity onPress={() => handleAdd()}>
+          <View style={styles.addWrapper}>
+          <FontAwesomeIcon icon={faPlus} style={styles.addText} />
+          </View>
+        </TouchableOpacity>
+
       </View>
-      <StatusBar style="auto" />
-    </View>
+    </View >
   );
 };
 
 const styles = StyleSheet.create({
-  statusBar: {
-    backgroundColor: "#7f39fb",
-    color: "#fff",
-    width: "100%",
-    height: 30
-  },
   container: {
     flex: 1,
-    backgroundColor: "#6554bb",
-    alignItems: "center",
-    justifyContent: "flex-start",
+    backgroundColor: '#E8EAED',
   },
-  app_background: {
-    backgroundColor: "6554bb",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    innerHeight: "100",
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
   },
-  main_container: {
-    backgroundColor: "#f5f6fa",
-    width: "500px",
-    height: "min-content",
-    borderradius: "5px",
-    padding: "20px",
-    boxshadow: "10px 10px 26px 0px rgba(0, 0, 0, 0.35)",
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
   },
-  title: {
-    textAlign: "center"
+  items: {
+    marginTop: 30,
   },
-  
-  add_item_box: {
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: 'center',
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center'
   },
-  
-  add_item_input: {
-    borderWidth:1,
-    borderColor: "#7f39fb",
-    borderRadius: 8,
-    padding: 10,
-    margin: 10,
-    width: "80%"
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
   },
-  
-  item_list: {
-    width: "100%",
-    display: "flex"
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
   },
-  
-  item_container: {
-    display: "flex",
-    alignitems: "center",
-    justifycontent: "space-between",
-    padding: "20px 0 20px 0",
-    borderbottom: "1px #6554bb solid"
+  addText: {},
+  item: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
   },
-  
-  item_name: {
-    cursor: "pointer"
+  itemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap'
   },
-
-  valor_item_box: {
-    display: "flex",
-    background: "#6554bb",
-    borderradius: "50px",
-    color: "#bab1e9",
-    display: "inline",
-    alignitems: "flex-end",
-    padding: "5px",
-    justifycontent: "right"
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: '#55BCF6',
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
   },
-  
-  
-  valor_item_input: {
-    border: "1px solid #6554bb",
-    background: "#6554bb",
-    borderradius: "50px",
-    color: "#c3bbe9",
-    minwidth: "70px",
-    width: "100px",
-    height: "40px"
+  itemText: {
+    maxWidth: '80%',
+    
   },
-  
-  item_action_buttons: {
-    /* width: 30%; */
-    display: "flex",
-    justifycontent: "space-between"
-  },
-  
-  remove_button: {
-    color: "#c3bbe9",
-    backgroundcolor: "red"
-  },
-  
-  total: {
-    float: "right",
-    padding: "10px"
-  },
-  
-  valor: {
-    float: "right",
-    padding: "10px"
-  },
-  
-  quantity: {
-    display: "flex",
-    alignitems: "center",
-    border: "1px solid #6554bb",
-    background: "#6554bb",
-    borderradius: "50px",
-    fontsize: "12px",
-    color: "#c3bbe9",
-    minwidth: "70px",
-    justifycontent: "space-between"
-  },
-  
-  button: {
-    background: "transparent",
-    border: "none",
-    margin: "3px",
-    color: "#c3bbe9"
-  },
-  
-  completed: {
+  itemText2: {
+    maxWidth: '80%',
     textDecorationLine: "line-through"
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: '#55BCF6',
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+  valor_box: {
+    display: 'flex',
+    borderRadius: '60px',
+
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'right',
+  },
+  quantity: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  valor: {
+    borderRadius: "50px",
+    minWidth: "70px",
+    width: "100px",
+    heigth: "40px",
+    backgroundColor: '#FFF',
+
   }
+
+
 });
 
 export default App;
